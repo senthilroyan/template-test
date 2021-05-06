8803c1b252de62f8d90a0b9cb026c622c2b14ba3
Senthil Royan <senthilroyan@gmail.com>
2021-05-06 13:46:17 +0530

    javaScript updates

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// Get Quote From API
+async function getQuote() {
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    console.log('response: ', response);
+    const data = await response.json();
+    console.log(data);
+
+      if (data.quoteAuthor === '') {
+        authorText.innerText = 'Unknown';
+      } else {
+        authorText.innerText = data.quoteAuthor;
+      }
+      if (data.quoteText.length > 120 ) {
+        quoteText.classList.add('long-quote');
+      } else {
+        quoteText.classList.remove('long-quote');
+      }
+  
+      quoteText.innerText = data.quoteText
+    
+
+  } catch (error) {
+    getQuote();
+    console.log('Whoops, no quote', error);
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} \n - ${author}`;
+  console.log('twitterUrl: ', twitterUrl)
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuote();
